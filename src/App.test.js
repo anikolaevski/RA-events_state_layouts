6dbfaff0cef63919e2fb4900334bc6663c06e106
Alexey Nikolaevskiy <anikolaevski@gmail.com>
2021-02-19 13:18:30 +0300

    fixed

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Tiger of Sweden/i);
+  const linkElement = screen.getByText(/red/i);
   expect(linkElement).toBeInTheDocument();
 });
